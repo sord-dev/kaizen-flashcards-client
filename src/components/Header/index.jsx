179a35b0e3de1d675d8b3066b76b0e69418d1e67
britishbambi <josefjakubiak@gmail.com>
2023-04-17 16:46:27 +0100

    optimise css on homepage

--- src/components/Header/index.jsx
+++ src/components/Header/index.jsx
@@ -13,8 +13,8 @@ export default function index() {
                 <NavLink to="stats" style={styles}>Statistics</NavLink>
                 <NavLink to="decks" style={styles}>Decks</NavLink>
                 <NavLink to="calender" style={styles}>Calender</NavLink>
-                <a href="#" ><img className='profile-img' src="https://static.vecteezy.com/system/resources/previews/007/033/146/original/profile-icon-login-head-icon-vector.jpg" alt="" /></a>
             </nav>
+            <a href="#" ><img className='profile-img' src="https://static.vecteezy.com/system/resources/previews/007/033/146/original/profile-icon-login-head-icon-vector.jpg" alt="" /></a>
         </header>
         <Outlet />
     </main>

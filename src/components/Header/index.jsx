62f7fc1c2087d737b241d29f7f588c2917b45f76
britishbambi <josefjakubiak@gmail.com>
2023-04-17 12:13:35 +0100

    Create study block on home page and begin styling

--- src/components/Header/index.jsx
+++ src/components/Header/index.jsx
@@ -1,15 +1,18 @@
 import React from 'react'
 import { NavLink, Outlet } from 'react-router-dom'
+const styles = ({isActive}) => ({
+    textDecoration: isActive ? 'underline #FAD97F' : 'none',
+})
 
 export default function index() {
   return (
     <main>
         <header>
             <nav>
-                <NavLink to="/" end>Home</NavLink>
-                <NavLink to="stats">Statistics</NavLink>
-                <NavLink to="decks">Decks</NavLink>
-                <NavLink to="calender">Calender</NavLink>
+                <NavLink to="/" style={styles} end>Home</NavLink>
+                <NavLink to="stats" style={styles}>Statistics</NavLink>
+                <NavLink to="decks" style={styles}>Decks</NavLink>
+                <NavLink to="calender" style={styles}>Calender</NavLink>
             </nav>
         </header>
         <Outlet />
--- src/pages/HomePage/index.jsx
+++ src/pages/HomePage/index.jsx
@@ -2,8 +2,15 @@ import React from 'react'
 
 export default function HomePage() {
   return (
-    <div>
-        <h1>Home Page</h1>
-    </div>
+    <>
+        <div>
+            <h1>おはよう, user</h1>
+        </div>
+        <div className='study-quota'>
+            <p className='quota-title'><span>Daily Study Quota</span></p>
+            <p>You're currently on a <span>4</span> day learning streak, keep it up!</p>
+            <a href='#'>Quick Study</a>
+        </div>
+    </>
   )
 }

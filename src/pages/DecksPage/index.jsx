03767ddddcef94e53276af35ba9db7c65d2abe95
sord-dev <75338985+sord-dev@users.noreply.github.com>
2023-04-18 19:38:18 +0100

    making POST request to /decks without token

--- src/pages/DecksPage/index.jsx
+++ src/pages/DecksPage/index.jsx
@@ -1,13 +1,16 @@
 import React, { useEffect, useState } from 'react'
 
 import { Table } from '../../components'
+import { useAuthContext } from '../../contexts/authContext'
 
 export default function DecksPage() {
+    const { user } = useAuthContext()
     const [decks, setDecks] = useState([])
 
     useEffect(() => { // get decks 
         const getDecks = async () => {
-            let decks = await (await fetch('http://localhost:3000/api/decks')).json()
+            let options = { method: "POST", headers: { 'Content-Type': 'application/json'}, body: JSON.stringify({user_id: user.user_id}) } 
+            let decks = await (await fetch("http://localhost:3000/deck", options)).json()
             setDecks(decks)
         }
 

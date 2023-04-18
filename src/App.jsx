0850e738d537a3e94ab5eca9c4cde6f29566e6b4
sord-dev <75338985+sord-dev@users.noreply.github.com>
2023-04-18 21:02:16 +0100

    adding basic styles for deck summary

--- src/App.jsx
+++ src/App.jsx
@@ -10,11 +10,14 @@ function App() {
     <>
       <Routes>
         <Route path="/" element={<Layout />} >
-            <Route index element={<Pages.HomePage />} />
+          <Route index element={<Pages.HomePage />} />
           <Route path='/login' element={<Pages.Login />} />
           <Route path='/register' element={<Pages.Register />} />
           <Route path='/stats' element={<Pages.StatsPage />} />
-          <Route path='/decks' element={<Pages.DecksPage />} />
+          <Route path='/decks' >
+            <Route index element={<Pages.DecksPage />} />
+            <Route path=':deck_id' element={<Pages.DeckSummary />} />
+          </Route>
         </Route>
         <Route path={'/*'} element={<Navigate to={'/'} />} />
       </Routes>

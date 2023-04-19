dcbaa9f1a3fdadd0b5dc08b9e3ef8a292c86e536
sord-dev <75338985+sord-dev@users.noreply.github.com>
2023-04-19 16:40:39 +0100

    create mutli use modal

--- src/pages/DeckPage/index.jsx
+++ src/pages/DeckPage/index.jsx
@@ -5,45 +5,60 @@ import { useNavigate, useParams } from 'react-router-dom'
 
 export default function DeckPage() {
     const navigate = useNavigate()
-    const [deck, setCards] = useState({ cards: [] })
+    const [deck, setDeck] = useState({ cards: [] })
     const { deck_id } = useParams()
-    const [openModal, setOpenModal] = useState()
+    const [openModal, setOpenModal] = useState(false)
+    const [newCardDetails, setNewCardDetails] = useState({ username: "", description: "", answer: "" })
     const { theme } = useTheme()
 
-    const addCard = (e) => {
-        e.preventDefault()
-        setOpenModal(true)
+    function handleCreateCard() {
+        console.log(newCardDetails) // REPLACE ME JACK C:
     }
 
     useEffect(() => {
         const getDeck = async () => {
             const deck = await (await fetch(`http://localhost:3000/deck/${deck_id}`)).json()
 
-            setCards(deck);
+            console.log(deck);
+            setDeck(deck);
         }
 
         getDeck()
     }, [])
+
+
 
     return (
         <div style={{ color: theme.primText }}>
             <h1>{deck.name}</h1>
             <p>{deck.cards.length} cards</p>
             <p>Start learning or add a card to expand your deck</p>
-            
+
             <div className='buttons-div'>
-                <button className='btnTheme' onClick={addCard}>+ Add Card</button>
+                <button className='btnTheme' onClick={() => setOpenModal(true)}>+ Add Card</button>
                 {deck?.cards.length ? <button className='btnTheme' onClick={() => navigate('learn')}>Start Learning</button> : null}
             </div>
 
-            {deck.cards.length ?
-                <div className='card-list'>
-                    {deck?.cards.map((card) => <Flashcard key={card.card_id} {...card} />)}
-                </div>
-                : <h3>Add some cards to study from above!</h3>}
+            {
+                deck ?
+                    <div className='card-list'>
+                        {deck?.cards.map((card) => <Flashcard key={card.card_id} {...card} />)}
+                    </div>
+                    :
+                    <h3>Add some cards to study from above!</h3>
+            }
 
             <div>
-                <Modal open={openModal} close={() => setOpenModal(false)} title='Add new card' buttonLabel='Add Card' />
+                <Modal open={openModal}>
+                    <h2>Add new Card</h2>
+                    <input value={newCardDetails.username} onChange={(e) => setNewCardDetails(prev => ({ ...prev, username: e.target.value }))} />
+                    <input value={newCardDetails.description} onChange={(e) => setNewCardDetails(prev => ({ ...prev, description: e.target.value }))} />
+                    <input value={newCardDetails.answer} onChange={(e) => setNewCardDetails(prev => ({ ...prev, answer: e.target.value }))} />
+                    <div className='btnContainer'>
+                        <button className='btnTheme' type='submit' id='btnAddDeck' onClick={() => handleCreateCard(newCardDetails.username, newCardDetails.description, newCardDetails.answer)}>Create Card</button>
+                        <button className='btnTheme' type='button' id='btnAddDeck' onClick={() => setOpenModal(false)}>Cancel</button>
+                    </div>
+                </Modal>
             </div>
         </div>
     )
--- src/pages/DecksPage/index.jsx
+++ src/pages/DecksPage/index.jsx
@@ -9,25 +9,26 @@ export default function DecksPage() {
 
     const [decks, setDecks] = useState([])
     const [openModal, setOpenModal] = useState()
-    
+    const [deckName, setDeckName] = useState()
+
     const addDecks = (e) => {
         e.preventDefault()
         setOpenModal(true)
     }
-    
+
     const handleCreateDeck = async (name) => {
         const deck = { user_id: user.user_id, name }
-        let options = { method: "POST", headers: { 'Content-Type': 'application/json'}, body: JSON.stringify(deck) } 
+        let options = { method: "POST", headers: { 'Content-Type': 'application/json' }, body: JSON.stringify(deck) }
         let deck_id = await (await fetch("http://localhost:3000/deck/new", options)).json()
 
         console.log(deck_id);
-        setDecks(prev => [...prev, {...deck, deck_id}]) 
+        setDecks(prev => [...prev, { ...deck, deck_id }])
         setOpenModal(false)
     }
 
     useEffect(() => { // get decks 
         const getDecks = async () => {
-            let options = { method: "POST", headers: { 'Content-Type': 'application/json'}, body: JSON.stringify({user_id: user.user_id}) } 
+            let options = { method: "POST", headers: { 'Content-Type': 'application/json' }, body: JSON.stringify({ user_id: user.user_id }) }
             let decks = await (await fetch("http://localhost:3000/deck", options)).json()
             console.log(decks);
             setDecks(decks)
@@ -40,13 +41,20 @@ export default function DecksPage() {
     return (
         <div>
             <button className='btnTheme' onClick={addDecks}>+ Add Decks</button>
-            
+
             <div className='deck-list'>
                 {decks.map(d => (<DeckCard key={d.deck_id} deck={d} />))}
             </div>
 
             <div>
-                <Modal open={openModal} close={() => setOpenModal(false)} title='Add new deck' addDeck={handleCreateDeck}></Modal>
+                <Modal open={openModal}>
+                    <h2>Add new deck</h2>
+                    <input id='newDeckInput' value={deckName} onChange={(e) => setDeckName(e.target.value)}></input>
+                    <div className='btnContainer'>
+                        <button className='btnTheme' type='submit' id='btnAddDeck' onClick={() => handleCreateDeck(deckName)}>Create Deck</button>
+                        <button className='btnTheme' type='button' id='btnAddDeck' onClick={() => setOpenModal(false)}>Cancel</button>
+                    </div>
+                </Modal>
             </div>
         </div>
     )

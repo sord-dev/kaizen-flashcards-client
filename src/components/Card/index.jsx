9019dae155f2da7fcb2305c399814bfada7b4fb2
britishbambi <josefjakubiak@gmail.com>
2023-04-19 14:06:19 +0100

    Begin card input

--- src/components/Card/index.jsx
+++ src/components/Card/index.jsx
@@ -8,6 +8,10 @@ export default function Card({ card_id, question, description, answer, show, onC
             </div>
 
             <div className={show ? styles["card-body-active"] : styles["card-body"]} >
+                // Create an input for the user to type in their answer
+                <input type="text" placeholder="Type your answer here" />
+                // Create a button for the user to submit their answer
+                <button>Submit</button>
                 <p>{answer}</p>
 
                 <p>{description}</p>

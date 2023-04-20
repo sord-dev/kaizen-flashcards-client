a089e247490455a5e5c674c0e8fadcdcace9d767
sord-dev <75338985+sord-dev@users.noreply.github.com>
2023-04-20 09:55:24 +0100

    adding dropdown styles and jsx

--- src/contexts/themes/index.jsx
+++ src/contexts/themes/index.jsx
@@ -2,11 +2,16 @@ import React, { useState, useContext, createContext } from "react";
 
 const ThemeContext = createContext();
 
+const themes = {
+    light: {'primColor': '#ffffff', 'SecColor': '#151723', 'primText': '#333333', 'secText': '#8ed1fc', 'primBG': '#E6E6E6', 'secBG': "#363842", 'accentColor': '#FAD97F', 'darkMode': false},
+    dark:  { 'primColor': '#151723', 'SecColor': '#ffffff', 'primText': '#8ed1fc', 'secText': '#333333', 'primBG': '#363842', 'secBG': "#F2F2F2", 'accentColor': '#FAD97F', 'darkMode': true }
+}
+
 export const ThemeProvider = ({ children }) => {
-    const [theme, setTheme] = useState({'primColor': '#ffffff', 'SecColor': '#151723', 'primText': '#333333', 'secText': '#8ed1fc', 'primBG': '#E6E6E6', 'secBG': "#363842", 'accentColor': '#FAD97F', 'darkMode': false});
+    const [theme, setTheme] = useState(themes.light);
 
     return (
-        <ThemeContext.Provider value={{ theme, setTheme }}>
+        <ThemeContext.Provider value={{ theme, setTheme, themes }}>
             {children}
         </ThemeContext.Provider>
     );
